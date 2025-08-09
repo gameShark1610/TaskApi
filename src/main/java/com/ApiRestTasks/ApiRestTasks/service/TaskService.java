7afdb34ee92f93f6fc5f434f7f72affac5d040c6
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2025-08-08 23:11:00 -0600

    Created taskService interface in Service

---
+++ src/main/java/com/ApiRestTasks/ApiRestTasks/service/TaskService.java
@@ -0,0 +1,14 @@
+package com.ApiRestTasks.ApiRestTasks.service;
+
+import com.ApiRestTasks.ApiRestTasks.model.Task;
+
+import java.util.List;
+
+//I thought in add a class but a good practice is use interfaces and create a carpet called imcp (interface + implementation)
+public interface TaskService {
+    List<Task> getAllTasks();
+    Task getTaskById(Integer id);
+    Task createTask(Task task);
+    Task updateTask(Integer id, Task taskDetails);
+    void deleteTask(Integer id);
+}
