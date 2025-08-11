a945f7dfb3fb771fd75c553dbe2784b189373b4f
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2025-08-11 00:19:36 -0600

    Adding DTO

---
+++ src/main/java/com/ApiRestTasks/ApiRestTasks/dto/respone/TaskRespondeDTO.java
@@ -0,0 +1,51 @@
+package com.ApiRestTasks.ApiRestTasks.dto.respone;
+
+import java.util.Date;
+
+public class TaskRespondeDTO {
+    private Integer id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private Date createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
