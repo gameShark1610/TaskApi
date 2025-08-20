9366e1530d27786107c65e6c88e55706fffe39f3
Yair Arciniega <yarciniegav2100@alumno.ipn.mx>
2025-08-19 22:51:32 -0600

    Add JwtAuthenticationEntryPoint (Forth Step to add JWT)

---
+++ src/main/java/com/ApiRestTasks/ApiRestTasks/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.ApiRestTasks.ApiRestTasks.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+/*
+Handles unauthenticated requests.
+If a client sends a request without a valid JWT, Spring Security calls this class. It responds with HTTP 401 Unauthorized, optionally with a message.
+*/
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    //Called automatically whenever an unauthenticated user tries to access a secured endpoint.
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        // Respond with 401 Unauthorized and a message
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: " + authException.getMessage());
+    }
+}
